d17989035087632521d1860a4deeec145e651053
yoshinobu-iriguchi <yoshinobu.iriguchi@scigineer.com>
2020-07-03 10:54:49 +0000

    バックアップとファイル周りのカスタマイズを追加。やってみたがちょっとじゃまなものをやめる。

--- custom.el
+++ custom.el
@@ -3,20 +3,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-notify-method (quote pushy))
+ '(backup-directory-alist (quote (("." . "~/backup"))))
  '(column-number-mode t)
  '(current-language-environment "UTF-8")
  '(default-input-method "japanese")
+ '(delete-by-moving-to-trash t)
+ '(delete-old-versions t)
  '(desktop-save-mode t)
  '(eol-mnemonic-dos "(DOS)")
  '(indent-tabs-mode nil)
- '(initial-buffer-choice ".")
+ '(inhibit-startup-screen t)
+ '(kept-new-versions 3)
  '(keyboard-coding-system (quote utf-8-dos))
- '(mode-require-final-newline (quote visit))
+ '(package-enable-at-startup t)
+ '(recentf-mode t)
  '(server-mode t)
  '(server-window (quote switch-to-buffer-other-frame))
+ '(show-paren-mode t)
  '(show-trailing-whitespace t)
  '(tab-width 4)
  '(tool-bar-mode nil)
+ '(version-control t)
  '(w32-system-shells (quote ("powershell.exe" "cmd" "cmd.exe"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
