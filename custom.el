0a194b051b8817307ced2f3fd4751db0518882dd
yoshinobu-iriguchi <yoshinobu.iriguchi@scigineer.com>
2020-07-31 10:38:05 +0000

    全角空白やタブ文字を見えるようにする

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(Man-notify-method (quote pushy))
+ '(backup-by-copying-when-linked t)
  '(backup-directory-alist (quote (("." . "~/backup"))))
  '(column-number-mode t)
  '(current-language-environment "UTF-8")
@@ -13,6 +14,7 @@
  '(describe-char-unicodedata-file "~/.emacs.d/UnicodeData.txt")
  '(desktop-save-mode t)
  '(eol-mnemonic-dos "(DOS)")
+ '(global-whitespace-mode t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(kept-new-versions 3)
@@ -25,7 +27,21 @@
  '(tab-width 4)
  '(tool-bar-mode nil)
  '(version-control t)
- '(w32-system-shells (quote ("powershell.exe" "cmd" "cmd.exe"))))
+ '(w32-system-shells (quote ("powershell.exe" "cmd" "cmd.exe")))
+ '(whitespace-display-mappings
+   (quote
+    ((space-mark 12288
+                 [9251]
+                 [164]
+                 [11036])
+     (space-mark 160
+                 [164]
+                 [95])
+     (newline-mark 10
+                   [36 10])
+     (tab-mark 9
+               [187 9]
+               [92 9])))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
